636b398887bd3be43b9543c36a8199fbd8f326b4
conghuynh040587-ship-it <conghuynh040587@gmail.com>
2026-09-06 18:21:41 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Thay cấu hình của thầy vào đây (lấy từ Firebase Console)
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+apiKey: "AIzaSyBqd_NK7P7nUSe77E0K08OMKe62wc9bvIU",
+  authDomain: "vatlythayhuynh.firebaseapp.com",
+  projectId: "vatlythayhuynh",
+  storageBucket: "vatlythayhuynh.firebasestorage.app",
+  messagingSenderId: "758843252383",
+  appId: "1:758843252383:web:d00b554666720089fb4caa",
+  measurementId: "G-3JPRECB4GQ"
 };
 
 const app = initializeApp(firebaseConfig);
