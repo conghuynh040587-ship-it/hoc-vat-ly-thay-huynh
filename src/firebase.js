1c7cd50c64265add29b8b89bffb130f7e528294a
conghuynh040587-ship-it <conghuynh040587@gmail.com>
2026-09-06 17:10:38 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
+// Thay cấu hình của thầy vào đây (lấy từ Firebase Console)
 const firebaseConfig = {
-  apiKey: "AIzaSyBqd_NK7P7nUSe77E0K08OMKe62wc9bvIU",
-  authDomain: "vatlythayhuynh.firebaseapp.com",
-  projectId: "vatlythayhuynh",
-  storageBucket: "vatlythayhuynh.firebasestorage.app",
-  messagingSenderId: "758843252383",
-  appId: "1:758843252383:web:d00b554666720089fb4caa",
-  measurementId: "G-3JPRECB4GQ"
+  apiKey: "AIzaSy...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const firestoreDb = getFirestore(app);
